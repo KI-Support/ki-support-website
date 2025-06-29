99a5fc807cecd0085cc2f274ca5d276bc837dd77
KI-Support <kermet87@web.de>
2025-06-29 04:09:24 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Pricing from './components/Pricing';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Pricing />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
